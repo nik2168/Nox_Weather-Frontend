6dbe349de1a043cabf47c7674fd551823791dd4d
nik2168 <128375663+nik2168@users.noreply.github.com>
2024-07-12 11:30:19 -0700

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,27 @@
-import React, { lazy, Suspense, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectRoute from "./Components/Auth/ProtectRoute";
+import { Skeleton } from "@mui/material";
 import axios from "axios";
+import React, { lazy, Suspense, useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ProtectRoute from "./Components/Auth/ProtectRoute";
+import { server } from "./Features/config";
+import NotFound from "./Pages/NotFound/NotFound";
+import { userExists, userNotExists } from "./redux/reducer/authslice";
 const Home = lazy(() => import("./Pages/Home/Home"));
 const Login = lazy(() => import("./Pages/Login/Login"));
 const DashBoard = lazy(() => import("./Pages/DashBoard/DashBoard"));
 const DefaultDashBoard = lazy(() =>
   import("./Pages/Default DashBoard/DefaultDashBoard")
 );
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { Skeleton } from "@mui/material";
-import NotFound from "./Pages/NotFound/NotFound";
-import { Toaster } from "react-hot-toast";
-import { server } from "./Features/config";
-import { userExists, userNotExists } from "./redux/reducer/authslice";
-import { useUserProfileQuery } from "./redux/api/api";
-import { useErrors } from "./Features/hooks";
 
 function App() {
   const { user, loader } = useSelector((state) => state.auth);
 
   const dispatch = useDispatch();
 
-  // const { isLoading, isError, error, data, refetch } = useUserProfileQuery();
-  // useErrors([{ isError, error }]);
-
-  // // if(isError){
-  // //   console.log(error)
-  // //         dispatch(userNotExists());
-  // // }
-
-  // dispatch(userExists(data?.user));
-  // }
-
-    //axios fetching ...
+  //axios fetching ...
   useEffect(() => {
     axios
       .get(`${server}/api/v1/user/profile`, { withCredentials: true })
