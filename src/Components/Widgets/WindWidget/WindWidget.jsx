1549c4cf3e85e2295ee4f1baab5c8aefb285cfca
nik2168 <128375663+nik2168@users.noreply.github.com>
2024-07-12 12:07:13 -0700

    some widget size fixes

--- src/Components/Widgets/WindWidget/WindWidget.jsx
+++ src/Components/Widgets/WindWidget/WindWidget.jsx
@@ -10,26 +10,26 @@ const WindWidget = ({curData}) => {
   return (
     <Box className="windWidgetBox">
       <div className="windWidgetHeader">
-        <Air sx={{ color: "whitesmoke", fontSize: "2.3rem", color: "skyblue" }} />
-        <Typography  sx={{ fontWeight: "600", fontSize: "1.3rem" }}>
+        <Air sx={{ color: "whitesmoke", fontSize: "2rem", color: "skyblue" }} />
+        <Typography  sx={{ fontWeight: "600", fontSize: "1rem" }}>
           Wind
         </Typography>
       </div>
       <Typography
-        variant="h4"
-        sx={{ fontWeight: "600", paddingBottom: "2rem", textAlign: "center" }}
+        variant="h5"
+        sx={{ fontWeight: "600", paddingBottom: "1rem", textAlign: "center" }}
       >
         {Math.trunc(curData?.wind?.speed)} KM/H
       </Typography>
       <div className="windWidgetHeader">
         <Directions sx={{ color: "whitesmoke" }} />
-        <Typography sx={{ fontWeight: "300", fontSize: "1rem" }}>
+        <Typography sx={{ fontWeight: "300", fontSize: "0.9rem" }}>
           Degree : {curData?.wind?.deg}°
         </Typography>
       </div>
       <div className="windWidgetHeader">
         <WaterDrop sx={{ color: "whitesmoke" }} />
-        <Typography sx={{ fontWeight: "300", fontSize: "1rem" }}>
+        <Typography sx={{ fontWeight: "300", fontSize: "0.9rem" }}>
           Precipitation : {curData?.main?.humidity}
         </Typography>
       </div>
